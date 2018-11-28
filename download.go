9ba64f36b520a6a5430f4260e082e5e9153a416b
Mwiza MBP 2017 <mwiza@agripredict.com>
2018-11-28 04:10:51 +0200

    Download descrete math notes

--- download.go
+++ download.go
@@ -1,2 +1,43 @@
 package main
 
+import (
+	"os"
+	"net/http"
+	"io"
+)
+
+func main() {
+
+	err := downloadFile()
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func downloadFile() error {
+	for i:=1; i<=24; i++ {
+		println (string(i))
+		url := "http://math.slu.edu/~freeman/Discrete_Lecture_"+ string(i) +".pdf"
+		// Create the file
+		out, err := os.Create(url)
+		if err != nil {
+			return err
+		}
+		defer out.Close()
+
+		// Get the data
+		resp, err := http.Get(url)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+
+		// Write the body to file
+		_, err = io.Copy(out, resp.Body)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
