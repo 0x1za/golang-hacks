72d98c306b58e554566783d58d441589df906a30
Mwiza Simbeye <mwiza@agripredict.com>
2019-07-29 02:02:21 +0200

    Done basic quiz with IO from csv

--- gophercises/quiz/solution.go
+++ gophercises/quiz/solution.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"encoding/csv"
+	"strconv"
+	"bufio"
 )
 
 func main()  {
@@ -11,13 +13,26 @@ func main()  {
 }
 
 func quiz(file_path string){
+	correct := 0
 	// Load the csv file from path
 	f, _ := os.Open(file_path)
 
 	// Create new reader.
 	r := csv.NewReader(f)
 	result, _ := r.ReadAll()
+	questions := strconv.Itoa(len(result))
+	fmt.Println("You have a total of " + questions +" questions")
+	// Start SDTIN reader.
+	scanner := bufio.NewScanner(os.Stdin)
 	for i := range result{
-		fmt.Println(result[i][0], result[i][1])
+		fmt.Print("What is "+result[i][0]+"? ")
+		answer := result[i][1]
+		scanner.Scan()
+		input := scanner.Text()
+
+		if (answer == input){
+			correct += 1
+		}
 	}
+	println("Your score is " + strconv.Itoa(correct) +"/"+questions)
 }
