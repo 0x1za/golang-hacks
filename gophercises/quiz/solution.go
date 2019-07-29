3e06f5515911fc73c116372b00f9fc4c97e890f3
Mwiza Simbeye <mwiza@agripredict.com>
2019-07-29 23:40:10 +0200

    Limit time on quiz

--- gophercises/quiz/solution.go
+++ gophercises/quiz/solution.go
@@ -6,6 +6,7 @@ import (
 	"encoding/csv"
 	"strconv"
 	"bufio"
+	"time"
 )
 
 func main()  {
@@ -21,17 +22,24 @@ func quiz(file_path string){
 	r := csv.NewReader(f)
 	result, _ := r.ReadAll()
 	questions := strconv.Itoa(len(result))
-	fmt.Println("You have a total of " + questions +" questions")
+	fmt.Println("You have a total of " + questions +" questions 😁")
 	// Start SDTIN reader.
 	scanner := bufio.NewScanner(os.Stdin)
+	start := time.Now()
 	for i := range result{
-		fmt.Print("What is "+result[i][0]+"? ")
-		answer := result[i][1]
-		scanner.Scan()
-		input := scanner.Text()
+		duration := int(time.Since(start).Seconds())
+		if duration < 30 {
+			fmt.Print("What is "+result[i][0]+"? ")
+			answer := result[i][1]
+			scanner.Scan()
+			input := scanner.Text()
 
-		if (answer == input){
-			correct += 1
+			if answer == input {
+				correct += 1
+			}
+		} else {
+			fmt.Println("Time Up!")
+			break
 		}
 	}
 	println("Your score is " + strconv.Itoa(correct) +"/"+questions)
